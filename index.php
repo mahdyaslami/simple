0e638f0a26fa0f0137fa24ed7334d6e5a7dc1e7b
mahdyaslami <mahdyaslami@gmail.com>
2020-05-03 12:41:57 +0430

    add try catch

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 require_once(__DIR__ . '/includes/global-variables.php');
 require_once(__DIR__ . '/includes/func-routing.php');
-require_once(__DIR__ . '/routes.php');
+
+try {
+    require_once(__DIR__ . '/routes.php');
+} catch (Throwable $e) {
+    http_response_code($e->code);
+    echo 'here';
+    throw $e;
+}
