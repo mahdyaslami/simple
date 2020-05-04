b96c177abd5a8bc716ae44ea3026419acd0874b7
mahdyaslami <mahdyaslami@gmail.com>
2020-05-04 09:47:54 +0430

    update exceptions thrown

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once(__DIR__ . '/includes/func-routing.php');
 try {
     require_once(__DIR__ . '/routes.php');
 } catch (Throwable $e) {
-    http_response_code($e->code);
-    echo 'here';
+    http_response_code($e->getCode());
     throw $e;
 }
