e223b75771518612d6c5b40339c261f8d5d52234
mahdyaslami <mahdyaslami@gmail.com>
2020-05-04 15:45:49 +0430

    Documenting (#3)
    * add exceptions and method
    
    * add try catch
    
    * update exceptions thrown
    
    * update readme
    
    * add comment to global var
    
    * add comment.

--- includes/global-variables.php
+++ includes/global-variables.php
@@ -1,7 +1,15 @@
 <?php
 
+//
+// If framework is in sub-directory add directories sequence here.
+//
+// http://www.google.com/path/to/site/ -> baseUri = /path/to/site
+//
 $baseUri = '/ssfw';
 
+//
+// Current requset object
+//
 $request = new stdClass();
 $request->uri = substr($_SERVER['REQUEST_URI'], strlen($baseUri));
 $request->method = $_SERVER['REQUEST_METHOD'];
