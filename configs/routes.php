ea4d4e206fe21e40b29ec0ddcc8aeef1897596f2
mahdyaslami <mahdyaslami@gmail.com>
2020-05-11 10:45:03 +0430

    check route parameters.

--- configs/routes.php
+++ configs/routes.php
@@ -1,12 +1,12 @@
 <?php
 
-$router->addRoute('GET', '/hello-world', function($parameters) {
+$router->addRoute('GET', '/hello-world', function() {
     require_once(__DIR__ . '/src/helloworld.php');
 });
 
 $router->addGroup('/users', function (FastRoute\RouteCollector $group) {
-    $group->addRoute('GET', '/{id:\d+}', function() {
-        echo 'an item';
+    $group->addRoute('GET', '/{id:\d+}[/{name}]', function($parameters) {
+        print_r($parameters);
     });
     $group->addRoute('POST', '/create', function() {
         echo 'create';
