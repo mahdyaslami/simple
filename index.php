902ab401c106d83fbf4fc4294046c1a19a223a05
mahdyaslami <mahdyaslami@gmail.com>
2020-05-11 09:07:53 +0430

    add autoload and update paths

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once(__DIR__ . '/includes/global-variables.php');
-require_once(__DIR__ . '/includes/class-router.php');
+require_once('includes/global-variables.php');
+require_once('includes/class-router.php');
+require_once('vendor/autoload.php');
 
 try {
-    require_once(__DIR__ . '/routes.php');
+    require_once('routes.php');
 } catch (Throwable $e) {
     http_response_code($e->getCode());
     throw $e;
