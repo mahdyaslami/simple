87bc68a622aeabb0d70295417e3e5d34efbe45ed
mahdyaslami <mahdyaslami@gmail.com>
2020-05-11 09:39:59 +0430

    remove querystring from uri

--- includes/global-variables.php
+++ includes/global-variables.php
@@ -12,4 +12,8 @@ $baseUri = '/ssfw';
 //
 $request = new stdClass();
 $request->uri = substr($_SERVER['REQUEST_URI'], strlen($baseUri));
+if (false !== $pos = strpos($request->uri, '?')) {
+    $request->uri = substr($request->uri, 0, $pos);
+}
+$request->uri = rawurldecode($request->uri);
 $request->method = $_SERVER['REQUEST_METHOD'];
