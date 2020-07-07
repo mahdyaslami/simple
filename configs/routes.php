7725be4bed075e0440f9382f2333b8a91bc12a65
unknown <Mahdi.Aslami@PART.LOCAL>
2020-07-07 13:10:34 +0430

    route item des

--- configs/routes.php
+++ configs/routes.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * An Route Item:
+ *  [
+ *      'method' => GET, POST, DELETE, ... have to be uppercase.
+ *      'path' => 
+ *              Path of url and have to start with `/` like `/users`.
+ *              Parameters have to be in `{}` like `/users/{id}`
+ *              Parameters name have to be in snake case format like `start-date`.
+ *      'params' => 
+ *              Array of parameters that used in path. see following array:
+ *                  [
+ *          'param-name' => 'regex-exp without parenthesis `()`'
+ *      ]
+ *  ]
+ */
 
 $routes = [
     [
