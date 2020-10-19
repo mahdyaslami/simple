51383828d1cd7b5de85d513d589f6d0d92d95323
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2020-10-19 13:05:57 +0330

    Change folder structure.

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-require_once('includes/global-variables.php');
-
 require_once('vendor/autoload.php');
 
-require_once('bootstrap/helpers.php');
-
-require_once('bootstrap/router.php');
+require_once('helpers.php');
