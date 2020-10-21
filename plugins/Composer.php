30391b81606cacb325029b0af480dbd4700606e4
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2020-10-21 15:19:10 +0330

    Refactor composer.php.

--- plugins/Composer.php
+++ plugins/Composer.php
@@ -4,19 +4,88 @@ namespace SSFW\Plugins;
 
 final class Composer
 {
-    public static function postAutoloadDump()
-    {
-        $installedPlugins = self::getInstalledPlugins();
-        $GLOBALS['installedPlugins'] = $installedPlugins;
+    const PLUGINS_JSON = 'plugins.json';
 
-        $GLOBALS['hasInstalledPlugin'] = false;
-        if (count($installedPlugins)) {
-            $GLOBALS['hasInstalledPlugin'] = true;
+    /**
+     * @var null|array<string>
+     */
+    protected static $plugins = null;
+
+    /**
+     * @var null|string
+     */
+    protected static $basePath = null;
+
+    /**
+     * Installation Manager.
+     */
+    protected static $installationManager = null;
+
+    /**
+     * List of installed packages.
+     * 
+     * @var array
+     */
+    protected static $packages = null;
+
+    /**
+     * Callback after autoloading.
+     */
+    public static function postAutoloadDump($event)
+    {
+        self::config($event);
+
+        foreach (self::$packages as $package) {
+            if (self::install($package)) {
+                echo "\"{$package}\" workspace installed successfully.\n";
+            }
         }
 
-        require_once(__DIR__ . '/../vendor/autoload.php');
-        
-        self::saveInstalledPlugins($GLOBALS['installedPlugins']);
+        self::save();
+    }
+
+
+    /**
+     * Base path of workspace.
+     * 
+     * @param string $path
+     * @return 
+     */
+    public static function basePath($path = '')
+    {
+        if (self::$basePath) {
+            $path = trim($path, '/\\');
+
+            return self::$basePath . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        }
+
+        self::$basePath = getcwd();
+
+        return self::basePath($path);
+    }
+
+    /**
+     * Get list of repositories.
+     */
+    protected static function config($event)
+    {
+        $composer = $event->getComposer();
+        $repositoryManager = $composer->getRepositoryManager();
+        self::$installationManager = $composer->getInstallationManager();
+        $localRepository = $repositoryManager->getLocalRepository();
+        self::$packages = $localRepository->getPackages();
+        self::getPlugins();
+    }
+
+    /**
+     * Get package install path.
+     * 
+     * @param string $package
+     * @return string
+     */
+    protected static function getInstallPath($package)
+    {
+        return self::$installationManager->getInstallPath($package);
     }
 
     /**
@@ -24,13 +93,62 @@ final class Composer
      * 
      * @return array<string>
      */
-    protected static function getInstalledPlugins()
+    protected static function getPlugins()
     {
-        return json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'plugins.json'));
+        if (self::$plugins) {
+            return self::$plugins;
+        }
+
+        $path = self::basePath(self::PLUGINS_JSON);
+
+        if (!file_exists($path)) {
+            return self::$plugins = [];
+        }
+
+        self::$plugins = json_decode(file_get_contents($path));
+
+        if (!self::$plugins) {
+            return self::$plugins = [];
+        }
+
+        return self::$plugins;
     }
 
-    protected static function saveInstalledPlugins($json) 
+    /**
+     * Install a package workspace.
+     * 
+     * @param  string $path
+     * @return bool
+     */
+    protected static function install($package)
     {
-        file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . 'plugins.json', json_encode($json));
+        $path = self::getInstallPath($package);
+
+        $installer = $path . DIRECTORY_SEPARATOR . 'installer.php';
+        if (!file_exists($path . DIRECTORY_SEPARATOR . 'installer.php')) {
+            return false;
+        }
+
+        if (in_array($package, self::$packages)) {
+            return false;
+        }
+
+        require_once($installer);
+
+        array_push(self::$plugins, $package);
+
+
+
+        return true;
+    }
+
+    /**
+     * Save installed plugins.
+     * 
+     * @return bool
+     */
+    protected static function save()
+    {
+        return file_put_contents(self::basePath(self::PLUGINS_JSON), json_encode(self::$plugins));
     }
 }
